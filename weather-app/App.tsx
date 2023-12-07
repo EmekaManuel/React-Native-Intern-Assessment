d68767ccb6bb53b28c6f7e0965d1ff02885af1ef
Onyewuchi Emeka <marrnuel123@gmail.com>
2023-12-07 11:47:57 +0100

    Update App.tsx

--- weather-app/App.tsx
+++ weather-app/App.tsx
@@ -2,7 +2,6 @@ import Navigation from "@/navigation";
 import theme from "@/utils/theme";
 import { ThemeProvider } from "@shopify/restyle";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import store from "@/modules";
@@ -19,11 +18,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
